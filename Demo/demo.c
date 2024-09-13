53635639274f7029a838b58a4aba9ca6d582fdd7
CoffeeBeann <iankcoffey@gmail.com>
2024-09-13 14:06:37 -0400

    Syncing files

---
+++ Demo/demo.c
@@ -0,0 +1,22 @@
+/***************************************************
+Filename: demo.c
+Author: MIDN Ian Coffey (m261194)
+demo for C
+***************************************************/
+
+// Import Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  double x, y;
+  printf("command: ");
+  while(scanf(" dist (%lf,%lf)",&x,&y) == 2) {
+    printf("%lf\n",(x*x + y*y));
+    printf("command: ");
+  }
+  printf("bye.\n");
+  return 0;
+}
